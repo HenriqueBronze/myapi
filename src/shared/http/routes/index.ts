a77efea39c710d81348537d854062d4f72dc0435
Bronze <joaobronze@sisconsultoria.com.br>
2024-01-08 19:27:12 -0300

    add errors middlleware

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import { AppError } from 'src/shared/errors/AppError';
+
 
 const routes = Router();
 
 routes.get('/', (request, response) => {
+  throw new AppError('deu ruim', 400);
   return response.json({ message: 'Olá Dev!' });
 });
 
