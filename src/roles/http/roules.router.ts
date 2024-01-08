94a36ba93f06b5adf16bae07d70e2775a2477f89
Bronze <joaobronze@sisconsultoria.com.br>
2024-01-08 19:59:24 -0300

    add role entity

--- src/roles/http/roules.router.ts
+++ src/roles/http/roules.router.ts
@@ -1,18 +1,19 @@
 import { Router } from 'express';
-import { v4 as uuidv4 } from 'uuid';
+import { Role } from '../entities/Roles';
+
 
 const rolesRouter = Router();
 
-const roles = [];
+const roles: Role[] = [];
 
 rolesRouter.post('/', (request, response) => {
   const { name } = request.body;
 
-  const role = {
-    id: uuidv4(),
+  const role = new Role();
+  Object.assign(role, {
     name,
     create_at: new Date(),
-  };
+  });
 
   roles.push(role);
 
