f96c9e47fb35990046a8e229260bd426d984b8c2
Bronze <joaobronze@sisconsultoria.com.br>
2024-01-08 19:47:14 -0300

    add roles

--- src/roles/http/routes/index.ts
+++ src/roles/http/routes/index.ts
@@ -1,13 +1,9 @@
 import { Router } from 'express';
-import { rolesRouter } from 'src/roles/http/roules.router';
-
 
 const routes = Router();
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'Olá Dev!' });
 });
-
-routes.use('/roles', rolesRouter);
 
 export { routes };
